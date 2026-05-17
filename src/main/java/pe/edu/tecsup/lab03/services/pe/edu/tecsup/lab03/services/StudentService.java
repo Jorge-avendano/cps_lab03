8448a65b9a1aa2acc05cdda551b5d17c65dbd709
Steven <steven@tecsup.edu.pe>
2026-05-17 03:14:47 -0500

    StudentRepository.java has been modified.

--- src/main/java/pe/edu/tecsup/lab03/services/pe/edu/tecsup/lab03/services/StudentService.java
+++ src/main/java/pe/edu/tecsup/lab03/services/pe/edu/tecsup/lab03/services/StudentService.java
@@ -1,4 +1,19 @@
 package pe.edu.tecsup.lab03.services.pe.edu.tecsup.lab03.services;
 
+import pe.edu.tecsup.lab03.entities.pe.edu.tecsup.lab03.entities.StudentEntity;
+import pe.edu.tecsup.lab03.repositories.pe.edu.tecsup.lab03.repositories.StudentRepository;
+import java.util.List;
+
 public class StudentService {
+    private StudentRepository studentRepository = new StudentRepository();
+
+    public void registerStudent(StudentEntity student) {
+        System.out.println("[Service] Procesando lógica para: " + student.getName());
+        // Aquí irían validaciones (ej. si el correo ya existe)
+        studentRepository.save(student);
+    }
+
+    public List<StudentEntity> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }
