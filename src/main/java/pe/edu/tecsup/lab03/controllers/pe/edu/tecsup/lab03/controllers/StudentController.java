3b1640216d2d411f3835d4f8410e92b2b17f546a
Steven <steven@tecsup.edu.pe>
2026-05-17 03:11:31 -0500

    StudentController.java has been modified.

--- src/main/java/pe/edu/tecsup/lab03/controllers/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,4 +1,14 @@
 package pe.edu.tecsup.lab03.controllers.pe.edu.tecsup.lab03.controllers;
 
+import pe.edu.tecsup.lab03.entities.pe.edu.tecsup.lab03.entities.StudentEntity;
+import pe.edu.tecsup.lab03.services.pe.edu.tecsup.lab03.services.StudentService;
+
 public class StudentController {
+    private StudentService studentService = new StudentService();
+
+    public void createStudent(Long id, String name, String email) {
+        System.out.println("[Controller] Petición HTTP recibida para crear estudiante");
+        StudentEntity newStudent = new StudentEntity(id, name, email);
+        studentService.registerStudent(newStudent);
+    }
 }
