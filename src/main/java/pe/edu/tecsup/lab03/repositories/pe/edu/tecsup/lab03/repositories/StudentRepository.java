6f8968ed8d16a17b91e8a6b01994bed6e7e50c21
Steven <steven@tecsup.edu.pe>
2026-05-17 03:13:52 -0500

    StudentRepository.java has been modified.

--- src/main/java/pe/edu/tecsup/lab03/repositories/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/repositories/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -1,4 +1,18 @@
 package pe.edu.tecsup.lab03.repositories.pe.edu.tecsup.lab03.repositories;
 
+import pe.edu.tecsup.lab03.entities.pe.edu.tecsup.lab03.entities.StudentEntity;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentRepository {
+    private List<StudentEntity> databaseMock = new ArrayList<>();
+
+    public void save(StudentEntity student) {
+        databaseMock.add(student);
+        System.out.println("[Repository] Estudiante guardado en BD: " + student.getName());
+    }
+
+    public List<StudentEntity> findAll() {
+        return databaseMock;
+    }
 }
